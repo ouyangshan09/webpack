06be4f0e4f51d97ec9e0f7f95f20f99ffcbd0ae8
Ouyangshan <ouyangshan09@gmail.com>
2017-03-01 11:10:33 +0800

    'meta.js'

--- meta.js
+++ meta.js
@@ -1,3 +1,25 @@
 /**
  * Created by OUYANG on 2017/3/1.
  */
+
+module.exports = {
+
+    "prompts": {
+        "name": {
+            "type": "string",
+            "required": true,
+            "message": "Project name"
+        },
+        "description": {
+            "type": "string",
+            "required": false,
+            "message": "Project description",
+            "default": "person skill project"
+        },
+        "author": {
+            "type": "string",
+            "message": "author"
+        },
+        "completeMessage": "To get started:\n\n {{^inPlace}}cd {{destDirName}}\n "
+    }
+};
